2e7ebb68b0db25bbffd6665b3944b8021f9e4754
Anco <danco2@illinois.edu>
2019-10-17 17:05:54 -0500

    init sequence ok, not sure about enable/disabel

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -11,16 +11,31 @@ uint8_t slave_mask;  /* IRQs 8-15 */
 
 /* Initialize the 8259 PIC */
 void i8259_init(void) {
+    // master_mask = 0xFF;
+    disable_irq(master_mask);
+    disable_irq(slave_mask);
+    outb(ICW1, MASTER_8259_PORT);
+    outb(ICW2_MASTER, MASTER_8259_PORT+1);
+    outb(ICW3_MASTER, MASTER_8259_PORT+1);
+    outb(ICW4, MASTER_8259_PORT+1);
+
+    outb(ICW1, SLAVE_8259_PORT);
+    outb(ICW2_SLAVE, SLAVE_8259_PORT+1);
+    outb(ICW3_SLAVE, SLAVE_8259_PORT+1);
+    outb(ICW4, SLAVE_8259_PORT+1);
 }
 
 /* Enable (unmask) the specified IRQ */
 void enable_irq(uint32_t irq_num) {
+  outb(irq_num, );
 }
 
 /* Disable (mask) the specified IRQ */
 void disable_irq(uint32_t irq_num) {
+  outb(irq_num, );
 }
 
 /* Send end-of-interrupt signal for the specified IRQ */
 void send_eoi(uint32_t irq_num) {
+  outb(EOI, );
 }
