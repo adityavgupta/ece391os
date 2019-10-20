c2d2b50785666801e7129a08827a4f5884d7e581
Darren <darrenanco@wirelessprv-10-192-235-92.near.illinois.edu>
2019-10-20 17:49:02 -0500

    changed system call dpl to 3

--- student-distrib/idt_init.c
+++ student-distrib/idt_init.c
@@ -179,7 +179,7 @@ void initialize_idt(void){
 	}
 
 	idt[SYS_CALL_INDEX].present=1; //Sets to present to indicate that is it present
-	idt[SYS_CALL_INDEX].dpl=0; //Sets the gate size to 32 bits
+	idt[SYS_CALL_INDEX].dpl=3; //Sets the gate size to 32 bits
 	idt[SYS_CALL_INDEX].reserved0=0; //These bits are set to idicate the field 0s111000000 where S is the size bit
 	idt[SYS_CALL_INDEX].size=1; //Sets the gate size to 32 bits
 	idt[SYS_CALL_INDEX].reserved1=1;
